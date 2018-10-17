57f80b812d3ef8da0a528b98aecd8155a4e43402
saillyMaxence <maxencesai@gmail.com>
2018-10-17 14:08:56 +0200

    Projet 003 - Ajout du session_destroy

--- include/header_inc.php
+++ include/header_inc.php
@@ -33,37 +33,71 @@
 
 <body id="page-top" class="index">
 
-    <!-- Navigation -->
-    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand" href="index.php">Micro blog</a>
-            </div>
+  <?php  if(isset($_SESSION['id'])){
+   ?>
+   <!-- Navigation -->
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span> Menu
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="index.php">Micro blog</a>
+        </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li class="page-scroll">
+                    <a href="include/deconnexion_inc.php">Déconnexion</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+   <?php
+  }
+  else{
+      ?>
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="index.php">Micro blog</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li class="page-scroll">
+                    <a href="Connexion.php">Connexion</a>
                     <li class="hidden">
                         <a href="#page-top"></a>
                     </li>
                     <li class="page-scroll">
-                        <a href="Connexion.php">Connexion</a>
-                        <li class="hidden">
-                            <a href="#page-top"></a>
-                        </li>
-                        <li class="page-scroll">
-                            <a href="inscription.php">Inscription</a>
-                        </ul>
-                    </div>
-                    <!-- /.navbar-collapse -->
+                        <a href="inscription.php">Inscription</a>
+                    </ul>
                 </div>
-                <!-- /.container-fluid -->
-            </nav>
-
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+  <?php
+  }
+  ?>
             <!-- Header -->
             <header>
                 <div class="container">
--- index.php
+++ index.php
@@ -5,35 +5,35 @@ session_start();
 <!DOCTYPE html>
 <html lang="fr">
 
-	<?php 
-		include("include/header_inc.php");
-	  	require("bdd/connection.php");
-	  	date_default_timezone_set('Europe/Paris');
-          ?>
+<?php 
+include("include/header_inc.php");
+require("bdd/connection.php");
+date_default_timezone_set('Europe/Paris');
+?>
 
-   <?php if(isset($_GET['id'])) {
+<?php if(isset($_GET['id'])) {
     // Inclusion de la modification
-    include("include/modify_inc.php");
- 
-    }
-    else {
+  include("include/modify_inc.php");
+  
+}
+else {
         //inclusion de l'insertion    
-     include("include/insert_inc.php");
-        }
-        ?>
-        
-
-    <?php 
-    if (isset($_SESSION['id'])){
-        include("include/displayConnected_inc.php");
-    }
-    else {
-        include("include/displayDisconnected_inc.php");
-    }
-     ?>           
+ include("include/insert_inc.php");
+}
+?>
 
 
-    <?php include("include/footer_inc.php"); ?>
+<?php 
+if (isset($_SESSION['id'])){
+  include("include/displayConnected_inc.php");
+}
+else {
+  include("include/displayDisconnected_inc.php");
+}
+?>           
+
+
+<?php include("include/footer_inc.php"); ?>
 
 </body>
 
